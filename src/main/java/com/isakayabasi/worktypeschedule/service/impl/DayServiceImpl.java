2b169585a7484ca25aa782d50ebb1d234506d998
ISA-KAYABASI <isakayabasi30@gmail.com>
2022-08-28 10:20:45 +0300

    update

--- src/main/java/com/isakayabasi/worktypeschedule/service/impl/DayServiceImpl.java
+++ src/main/java/com/isakayabasi/worktypeschedule/service/impl/DayServiceImpl.java
@@ -1,12 +1,11 @@
 package com.isakayabasi.worktypeschedule.service.impl;
 
 
-import com.isakayabasi.worktypeschedule.model.Day;
+
 import com.isakayabasi.worktypeschedule.repository.DayRepository;
 import com.isakayabasi.worktypeschedule.service.DayService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class DayServiceImpl implements DayService {
@@ -16,16 +15,6 @@ public class DayServiceImpl implements DayService {
     public DayServiceImpl(DayRepository dayRepository) {
         super();
         this.dayRepository = dayRepository;
-    }
-
-    @Override
-    public Day saveDay(Day day) {
-        return dayRepository.save(day);
-    }
-
-    @Override
-    public List<Day> getAllDays() {
-        return dayRepository.findAll();
     }
 
 
